cac1b1c320dccabf4dc23b4886a48d8e91847dd0
shravan <shravan3246@gmail.com>
2024-03-04 23:13:08 +0530

    all required installations are done

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+   <div>
+    <h1 className='text text-red600 text-center p-4 bg-orange-500'>welcome to react</h1>
+   </div>
+    
+  )
+}
+
+export default App
